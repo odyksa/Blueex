44d39f8961d17a6517ab2efe525f085888352f8c
odyksa <odyksa@myagent.com.ua>
2019-07-24 08:23:02 +0300

    add func without for react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 import Home from './pages/Home';
